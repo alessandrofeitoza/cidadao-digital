281051e7e0b08a6493aa90c0277d5c0669d0fdc1
Alessandro Feitoza <eu@alessandrofeitoza.eu>
2020-06-11 19:36:42 -0300

    Create a content to desafio01

---
+++ desafio01/index.php
@@ -0,0 +1,42 @@
+<?php
+    require_once 'config.php';
+
+    function render(string $filename): void
+    {
+        include_once "partials/{$filename}.php";
+    }
+?>
+
+<!DOCTYPE html>
+<!--
+	Astral by HTML5 UP (Edited by Alessandro Feitoza)
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html lang="pt-br">
+    <?php render('head'); ?>
+	<body class="is-preload">
+        <div id="wrapper">
+            <?php render('menu'); ?>
+
+            <!-- Main -->
+            <div id="main">
+                <?php
+                    render('article01');
+                    render('article02');
+                    render('article03');
+                ?>
+            </div>
+
+            <?php render('footer'); ?>
+        </div>
+
+		<!-- Scripts -->
+        <script src="/desafio01/assets/js/jquery.min.js"></script>
+        <script src="/desafio01/assets/js/browser.min.js"></script>
+        <script src="/desafio01/assets/js/breakpoints.min.js"></script>
+        <script src="/desafio01/assets/js/util.js"></script>
+        <script src="/desafio01/assets/js/main.js"></script>
+
+	</body>
+</html>
